1dc83ee23ec4c2973e79d5da45ac144448c17b7b
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-26 01:49:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -53,15 +53,6 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-# def self.new_from_db(row)
-#   new_song = self.new  # self.new is the same as running Song.new
-#   new_song.id = row[0]
-#   new_song.name =  row[1]
-#   new_song.length = row[2]
-#   new_song  # return the newly created instance
-# end
-
-
   def self.new_from_db(row)
     student = self.new(row[1],row[2],row[0])
     @id = row[0]
